a55c625e293858c6ad6e03f9622009288ead32be
Hakan Lindestaf <hakan@lindestaf.com>
2011-08-16 20:28:57 -0700

    Add update service

---
+++ src/UpdateService/MainService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace UpdateService
+{
+    public partial class MainService : ServiceBase
+    {
+        public MainService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
